b2a87aa2535b901d4b488b47d3090dda08925901
Eugene Prilepin <esp.home@gmail.com>
2019-08-27 03:00:06 +0300

    Add blank src files and setup.py script

---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+from setuptools import setup
+
+
+ROOT_DIR = pathlib.Path(__file__).parent
+PKG_NAME = 'curve'
+
+
+def get_version():
+    version_info = {}
+    version_file = ROOT_DIR / PKG_NAME / '_version.py'
+    with version_file.open() as f:
+        exec(f.read(), version_info)
+    return version_info['__version__']
+
+
+def get_long_description():
+    readme_file = ROOT_DIR / 'README.md'
+    return readme_file.read_text(encoding='utf-8')
+
+
+setup(
+    name='scikit-curve',
+    version=get_version(),
+    python_requires='>=3.5,<4',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    packages=[PKG_NAME],
+    url='https://github.com/espdev/scikit-curve',
+    license='BSD 3-Clause',
+    author='Eugene Prilepin',
+    author_email='esp.home@gmail.com',
+    description='A set of tools to manipule curves in Python',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
