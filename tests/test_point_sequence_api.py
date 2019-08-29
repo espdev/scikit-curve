536163c507fda6f232f53da08ded27bf4d2ced6d
Eugene Prilepin <esp.home@gmail.com>
2019-08-30 01:07:43 +0300

    Add tests for Point sequence api

---
+++ tests/test_point_sequence_api.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import numpy as np
+
+from curve import Point
+
+
+@pytest.mark.parametrize('data, ndim, dtype', [
+    ([1], 1, np.int32),
+    ([1, 2], 2, np.float64),
+    ([1, 2, 3], 3, np.float32),
+])
+def test_construct(data, ndim, dtype):
+    """Tests creating the instance of 'Curve' class
+    """
+    point = Point(data, dtype=dtype)
+
+    assert len(point) == ndim
+    assert point.ndim == ndim
+    assert point.dtype == dtype
+
+
+def test_eq():
+    p1 = Point([1, 2])
+    p2 = Point([1, 2])
+    assert p1 == p2
+
+
+def test_eq_integer():
+    p1 = Point([1, 2], dtype=int)
+    p2 = Point([1, 2], dtype=int)
+    assert p1 == p2
+
+
+@pytest.mark.parametrize('point_data', [
+    [1, 2, 3],
+    [2, 3, 4, 5],
+])
+def test_ne(point_data):
+    p1 = Point([1, 2, 3, 4])
+    p2 = Point(point_data)
+    assert p1 != p2
+
+
+def test_reversed():
+    point = Point([1, 2, 3])
+    assert reversed(point) == Point([3, 2, 1])
+
+
+@pytest.mark.parametrize('index, expected_data', [
+    (0, 1),
+    (1, 2),
+    (-1, 4),
+    (-2, 3),
+])
+def test_get_item(index, expected_data):
+    point = Point([1, 2, 3, 4])
+    assert point[index] == expected_data
