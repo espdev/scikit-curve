2c1d43542319962da16d5618b5ced64f0783017f
Eugene Prilepin <esp.home@gmail.com>
2019-08-30 01:08:46 +0300

    Refactoring: delete _types module

--- curve/_base.py
+++ curve/_base.py
@@ -6,8 +6,28 @@ import textwrap
 
 import numpy as np
 
-from ._types import PointDataType, CurveDataType, DataType
 
+NumberType = t.Union[int, float]
+
+PointDataType = t.Union[
+    t.Sequence[NumberType],
+    np.ndarray,
+    'Point',
+]
+
+CurveDataType = t.Union[
+    t.Sequence[t.Sequence[NumberType]],
+    t.Sequence[np.ndarray],
+    t.Sequence['Point'],
+    np.ndarray,
+    'Curve',
+]
+
+DataType = t.Union[
+    t.Type[int],
+    t.Type[float],
+    np.dtype,
+]
 
 IndexerType = t.Union[
     int,
