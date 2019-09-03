3ce203fb48fb73c6a078aba8a68d20b47a14544b
Eugene Prilepin <esp.home@gmail.com>
2019-09-03 13:03:44 +0300

    Add import CurvePoint from curve-package

--- curve/__init__.py
+++ curve/__init__.py
@@ -2,12 +2,13 @@
 
 from ._version import __version__  # noqa
 
-from ._base import Axis, Point, Curve
+from ._base import Axis, Point, CurvePoint, Curve
 from .distance import known_metrics, get_metric
 
 __all__ = [
     'Axis',
     'Point',
+    'CurvePoint',
     'Curve',
     'known_metrics',
     'get_metric',
