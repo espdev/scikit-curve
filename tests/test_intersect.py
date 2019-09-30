d2f7c55e66bb109870054ef4a0ed964abdbe2fda
Eugene Prilepin <esp.home@gmail.com>
2019-09-30 16:13:28 +0300

    Add tests for 3d, fix test for 2d

--- tests/test_intersect.py
+++ tests/test_intersect.py
@@ -5,25 +5,53 @@ from curve import Curve, Point
 
 
 @pytest.mark.parametrize('data1, data2, segments1, segments2, intersect_points', [
+    # ----------------------
+    # 2D
+
     # no intersections
     ([(1, 2, 3, 4), (1, 2, 3, 4)], [(4, 5, 6, 7), (7, 6, 5, 4)], [], [], []),
     # parallel, no intersections
     ([(1, 2, 3, 4), (1, 2, 3, 4)], [(1.5, 2.5, 3.5, 4.5), (0.5, 1.5, 2.5, 3.5)], [], [], []),
     # 1 pt
     ([(1, 2, 3, 4), (1, 2, 3, 4)], [(2, 3, 4, 5), (5, 4, 3, 2)], [2], [1], [Point([3.5, 3.5])]),
-    # overlap
+    # overlap 1 pt
     ([(1, 2), (1, 2)], [(0, 3), (0, 3)], [0], [0], [Point([1.5, 1.5])]),
+    # overlap 1 pt
+    ([(1, 2), (1, 2)], [(2, 3), (2, 3)], [0], [0], [Point([2, 2])]),
     # overlap 3 pt
     ([(1, 2, 3), (1, 2, 3)], [(0, 2.5, 4), (0, 2.5, 4)], [0, 1, 1], [0, 0, 1], [Point([1.5, 1.5]),
                                                                                 Point([2.25, 2.25]),
                                                                                 Point([2.75, 2.75])]),
-    # overlap 2 pt
-    ([(1, 2), (1, 2)], [(2, 3), (2, 3)], [0, 1], [0, 1], [Point([2, 2]), Point([2, 2])]),
+    # overlap 5 pt
+    ([(1, 2, 3, 4), (1, 2, 3, 4)],
+     [(0.5, 2.5, 3.5, 4.5), (0.5, 2.5, 3.5, 4.5)], [0, 1, 1, 2, 2], [0, 0, 1, 1, 2], [Point([1.5, 1.5]),
+                                                                                      Point([2.25, 2.25]),
+                                                                                      Point([2.75, 2.75]),
+                                                                                      Point([3.25, 3.25]),
+                                                                                      Point([3.75, 3.75])]),
     # orthogonal curves
     ([(2.5, 2.5, 2.5, 2.5), (1, 2, 3, 4)], [(1, 2, 3, 4), (2.5, 2.5, 2.5, 2.5)], [1], [1], [Point([2.5, 2.5])]),
     # self intersect
     ([(1, 2, 3, 3, 2.5, 2.5, 2.5, 2.5), (1, 1, 1, 2, 2, 1.5, 0.5, 0)], None, [1], [5], [Point([2.5, 1.0])]),
     ([(1, 2, 3, 2, 1), (1, 0, 1, 3, 1)], None, [0], [3], [Point([1.0, 1.0])]),
+
+    # ----------------------
+    # 3D
+
+    # no intersections
+    ([(1, 2, 3, 4), (1, 2, 3, 4), (1, 2, 3, 4)],
+     [(1, 2, 3, 4), (4, 3, 2, 1), (2, 3, 4, 5)], [], [], []),
+    # 1 pt
+    ([(1, 2, 3, 4), (1, 2, 3, 4), (1, 2, 3, 4)],
+     [(1, 2, 3, 4), (4, 3, 2, 1), (1, 2, 3, 4)], [1], [1], [Point([2.5, 2.5, 2.5])]),
+    # overlap 5 pt
+    ([(1, 2, 3, 4), (1, 2, 3, 4), (1, 2, 3, 4)],
+     [(0.5, 2.5, 3.5, 4.5), (0.5, 2.5, 3.5, 4.5), (0.5, 2.5, 3.5, 4.5)],
+     [0, 1, 1, 2, 2], [0, 0, 1, 1, 2], [Point([1.5, 1.5, 1.5]),
+                                        Point([2.25, 2.25, 2.25]),
+                                        Point([2.75, 2.75, 2.75]),
+                                        Point([3.25, 3.25, 3.25]),
+                                        Point([3.75, 3.75, 3.75])]),
 ])
 def test_curves_intersect(data1, data2, segments1, segments2, intersect_points):
     if data2:
@@ -35,8 +63,7 @@ def test_curves_intersect(data1, data2, segments1, segments2, intersect_points):
         curve = Curve(data1)
         intersections = curve.intersect()
 
-    if not segments1:
-        assert len(intersections) == 0
+    assert len(intersections) == len(segments1)
 
     for i, intersection in enumerate(intersections):
         assert segments1[i] == intersection.segment1.idx
