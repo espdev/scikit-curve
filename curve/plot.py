0a13890adb33754883f95f6eda4058e86371ff70
Eugene Prilepin <esp.home@gmail.com>
2019-10-14 23:47:06 +0300

    Rename 'plot' to 'curveplot' back

--- curve/plot.py
+++ curve/plot.py
@@ -15,7 +15,7 @@ if ty.TYPE_CHECKING:
     from curve import Curve
 
 
-class plot:
+class curveplot:
     """Plots a curve
 
     The function plots 2-d or 3-d curve using matplotlib.
@@ -61,14 +61,14 @@ class plot:
     def axes(self) -> plt.Axes:
         return self._axes
 
-    def plot(self,
-             curve: 'Curve',
-             *args: ty.Any,
-             param: ty.Optional[str] = None,
-             param_cmap: str = 'plasma',
-             show_normals: bool = False,
-             **kwargs: ty.Any) -> 'plot':
-        return plot(
+    def curveplot(self,
+                  curve: 'Curve',
+                  *args: ty.Any,
+                  param: ty.Optional[str] = None,
+                  param_cmap: str = 'plasma',
+                  show_normals: bool = False,
+                  **kwargs: ty.Any) -> 'plot':
+        return curveplot(
             curve,
             *args,
             param=param,
